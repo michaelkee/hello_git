b233a7639548e3835756b096f3c710f75a7fd76b
michael kee <mchl.kee@gmail.com>
2018-04-13 16:41:20 -0500

    added decorator

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
 import sys
 
+def my_decorator(f):
+    def func(*args, **kwargs):
+        return print(f(*args, **kwargs) + ' ' + sys.argv[-1])
+    return func
+
+@my_decorator
+def greeting():
+    return 'hello'
+
 if __name__ == '__main__':
-    print('hello git')
-    print('hello %s' % sys.argv[-1])
+    greeting()
